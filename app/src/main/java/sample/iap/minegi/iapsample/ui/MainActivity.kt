a689b923d8a7967ac44a8b9ad9e027f9402dd18f
Neil <neil@hpcnt.com>
2017-08-30 16:38:18 +0900

    isBillingSupported() API 추가 버튼 스타일 및 색상 추가 isBillingSupported 버튼 추가 Activity 방향을 세로로 고정

--- app/src/main/java/sample/iap/minegi/iapsample/ui/MainActivity.kt
+++ app/src/main/java/sample/iap/minegi/iapsample/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package sample.iap.minegi.iapsample.ui
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import sample.iap.minegi.iapsample.R
 import sample.iap.minegi.iapsample.databinding.ActivityMainBinding
 
@@ -12,7 +13,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initializeBindingData()
     }
 
+    private fun initializeBindingData() {
+        binding.activity = this
+        binding.textMyCoin.text = "0"
+    }
 
+    fun isBillingSupported() {
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+    }
 }
