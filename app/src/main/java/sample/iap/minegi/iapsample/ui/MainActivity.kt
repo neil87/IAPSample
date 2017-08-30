1f4210be9827f5ecd75d804822d0d55a596fd5f6
Neil <neil@hpcnt.com>
2017-08-30 15:49:13 +0900

    컴파일 에러 수정

--- app/src/main/java/sample/iap/minegi/iapsample/ui/MainActivity.kt
+++ app/src/main/java/sample/iap/minegi/iapsample/ui/MainActivity.kt
@@ -1,13 +1,18 @@
 package sample.iap.minegi.iapsample.ui
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import sample.iap.minegi.iapsample.R
+import sample.iap.minegi.iapsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
+
 }
